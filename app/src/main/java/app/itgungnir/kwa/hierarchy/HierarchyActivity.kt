a96f9e29a47a9bbe8e0f080136430baad1bcaa45
ITGungnir <wangzhiyu@hyperchain.cn>
2019-05-06 16:52:10 +0800

    route constant

--- app/src/main/java/app/itgungnir/kwa/hierarchy/HierarchyActivity.kt
+++ app/src/main/java/app/itgungnir/kwa/hierarchy/HierarchyActivity.kt
@@ -3,13 +3,14 @@ package app.itgungnir.kwa.hierarchy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import app.itgungnir.kwa.R
+import app.itgungnir.kwa.common.HierarchyActivity
 import app.itgungnir.kwa.tree.TreeState
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_hierarchy.*
 import my.itgungnir.apt.router.annotation.Route
 import my.itgungnir.rxmvvm.core.mvvm.BaseActivity
 
-@Route("hierarchy")
+@Route(HierarchyActivity)
 class HierarchyActivity : BaseActivity() {
 
     override fun layoutId(): Int = R.layout.activity_hierarchy
