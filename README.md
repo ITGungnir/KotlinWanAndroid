677958e0f9156aae7e1c8e1e44345b3e5fcab545
ITGungnir <wangzhiyu@hyperchain.cn>
2019-05-08 00:06:00 +0800

    prepare icons

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # KotlinWanAndroid
 WanAndroid client programmed with Kotlin.
 
-## 功能
-- 我的页面（含我的收藏、登录、注册、退出登录）
-
 ## ToFix
+* 我的页面：登录、注册、我的收藏
 * Web页面：收藏功能
+* 设置页面：退出登录
+* 设置页面：自动缓存
+* 设置页面：清除缓存
+* 设置页面：无图模式
+* 设置页面：夜间模式
--- app/src/main/java/app/itgungnir/kwa/mine/MineFragment.kt
+++ app/src/main/java/app/itgungnir/kwa/mine/MineFragment.kt
@@ -1,12 +1,24 @@
 package app.itgungnir.kwa.mine
 
 import app.itgungnir.kwa.R
+import app.itgungnir.kwa.common.widget.easy_adapter.bind
+import kotlinx.android.synthetic.main.fragment_mine.*
 import my.itgungnir.rxmvvm.core.mvvm.BaseFragment
 
 class MineFragment : BaseFragment() {
+
     override fun layoutId(): Int = R.layout.fragment_mine
 
     override fun initComponent() {
+
+        headBar.title("我的")
+            .addToolButton("\ue6de") {
+                // TODO TODO
+            }.addToolButton("\ue728") {
+                // TODO 设置
+            }
+
+        list.bind()
     }
 
     override fun observeVM() {
